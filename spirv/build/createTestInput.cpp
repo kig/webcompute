a53fc666ce3dc5f31b3f18a51c299e093ebf7ee7
Ilmari Heikkinen <hei@heichen.hk>
2018-12-18 10:34:01 +0800

    select vulkan device for kernel, build script tweaks

--- spirv/build/createTestInput.cpp
+++ spirv/build/createTestInput.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    int32_t header[4] = {3200 * 2400 * 4 * 4, 100, 75, 1};
+    int32_t header[5] = {3200 * 2400 * 4 * 4, 0, 100, 75, 1};
     float dims[2] = {3200.0f, 2400.0f};
     fwrite(header, sizeof(header), 1, stdout);
     fwrite(dims, sizeof(dims), 1, stdout);
